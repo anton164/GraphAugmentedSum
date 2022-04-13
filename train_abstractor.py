52e88ea4779a2a63b58bc69d1c8009ad7f14f88b
Anton Abilov <anton.abilov@gmail.com>
2022-04-12 20:00:01 -0400

    Reduced number of cpu workers to 2

--- train_abstractor.py
+++ train_abstractor.py
@@ -252,14 +252,14 @@ def build_batchers(word2id, cuda, debug):
     train_loader = DataLoader(
         MatchDataset_all2all('train'), batch_size=BUCKET_SIZE,
         shuffle=not debug,
-        num_workers=4 if cuda and not debug else 0,
+        num_workers=2 if cuda and not debug else 0,
         collate_fn=coll_fn
     )
     train_batcher = BucketedGenerater(train_loader, prepro, sort_key, batchify,
                                       single_run=False, fork=not debug)
     val_loader = DataLoader(
         MatchDataset_all2all('val'), batch_size=BUCKET_SIZE,
-        shuffle=False, num_workers=4 if cuda and not debug else 0,
+        shuffle=False, num_workers=2 if cuda and not debug else 0,
         collate_fn=coll_fn
     )
     val_batcher = BucketedGenerater(val_loader, prepro, sort_key, batchify,
@@ -281,14 +281,14 @@ def build_batchers_bert(cuda, debug, bert_model):
     train_loader = DataLoader(
         SumDataset('train'), batch_size=BUCKET_SIZE,
         shuffle=not debug,
-        num_workers=4 if cuda and not debug else 0,
+        num_workers=2 if cuda and not debug else 0,
         collate_fn=coll_fn
     )
     train_batcher = BucketedGenerater(train_loader, prepro, sort_key, batchify,
                                       single_run=False, fork=not debug)
     val_loader = DataLoader(
         SumDataset('val'), batch_size=BUCKET_SIZE,
-        shuffle=False, num_workers=4 if cuda and not debug else 0,
+        shuffle=False, num_workers=2 if cuda and not debug else 0,
         collate_fn=coll_fn
     )
     val_batcher = BucketedGenerater(val_loader, prepro, sort_key, batchify,
